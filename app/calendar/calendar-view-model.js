56bf09a08991ddbcada28fb1080229345908d8c3
ciro97 <cirogiuseppe.devita001@studenti.uniparthenope.it>
2019-06-28 01:36:40 +0200

    calendar

--- app/calendar/calendar-view-model.js
+++ app/calendar/calendar-view-model.js
@@ -1,13 +1,15 @@
 const observableModule = require("tns-core-modules/data/observable");
+const calendarModule = require("nativescript-ui-calendar");
 
 const SelectedPageService = require("../shared/selected-page-service");
+let calendar;
 
 function CalendarViewModel() {
     SelectedPageService.getInstance().updateSelectedPage("Calendar");
 
     const viewModel = observableModule.fromObject({});
 
-
+    //calendar = page.getViewById("myCalendar");
 
     return viewModel;
 }
